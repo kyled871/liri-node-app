0f1b16f6eb0cfe377d918aa2b20c7c7dec9ae39c
Kyle Davis <kyled871@yahoo.com>
2020-06-02 18:15:38 -0400

    added commands and inputs started concert function

--- liri.js
+++ liri.js
@@ -6,9 +6,21 @@ var axios = require("axios");
 var moment = require("moment");
 var omdb = new omdb(keys.omdb);
 var spotify_api = require("node-spotify-api");
-var input_command = process.argv;
+var command = process.argv[2];
+var input = process.argv.slice(3).join(" ");
 
 var spotify = new Spotify(keys.spotify);
 
 
 
+
+
+let concertThis = (artist) => {
+
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/?app_id=codingbootcamp"
+
+
+
+
+
+}
