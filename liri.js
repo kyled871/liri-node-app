396791cb53517c1f5eac020cd02973bc51fb775e
Kyle Davis <kyled871@yahoo.com>
2020-06-02 17:34:24 -0400

    added global variables and dependencies to liri

--- liri.js
+++ liri.js
@@ -1,3 +1,14 @@
+// dependecies and glolbal variables
 require("dotenv").config();
+var fs = require("fs");
 var keys = require("./keys.js");
+var axios = require("axios");
+var moment = require("moment");
+var omdb = new omdb(keys.omdb);
+var spotify_api = require("node-spotify-api");
+var input_command = process.argv;
+
 var spotify = new Spotify(keys.spotify);
+
+
+
